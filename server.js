6adcd74073e5764fd4bd681b2f22eb11e1d5d077
Angel Jimenez de Cisneros <angeljcc@gmail.com>
2015-04-05 19:32:41 +0000

    sockets

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ var http = require('http').Server(app);
 
 
 app.get('/',function(req,res){
-   res.sendFile(__dirname + '/index.html');
+   res.sendFile(__dirname + '/view/index.html');
 });
 
-http.listen(3000,function(){
+var server = http.listen(3000,function(){
    console.log('escuchando en http://localhost:3000');
+});
+var click = 0;
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection',function(socket){
+    socket.emit('conectado');
+
+    socket.on('sendMessage',function(data){
+       io.sockets.emit('newMessage',{msg: data});
+    });
+
+
+
+
 });
